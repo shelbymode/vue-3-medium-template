b72195abb372ecac9c2655747695b97ae9057c1a
Shelbymode <pocketbook.love24@gmail.com>
2024-02-22 22:55:49 +0300

    feat: global refactor

---
+++ src/shared/ui/organismes/global-header.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { VAtomToggleDarkMode } from '../atoms/switches'
+</script>
+
+<template>
+  <header class="header">
+    <nav class="link-container">
+      <router-link class="link" to="/">Home</router-link>
+      <router-link class="link" to="/qrcode-generator">Qr code generator</router-link>
+      <router-link class="link" to="/qrcode-reader">Qr code reader</router-link>
+      <router-link class="link" to="/intersection-api/reactive-aside-content">Reactive aside content</router-link>
+      <router-link class="link" to="/intersection-api/infinity-scroll">Infinity Scroll</router-link>
+
+      <VAtomToggleDarkMode class="header__theme-toggler" />
+    </nav>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  @apply flex flex-col items-center;
+  @apply w-full;
+}
+.link-container {
+  @apply flex items-center justify-center gap-x-6;
+  @apply w-full;
+  @apply bg-black text-white;
+  @apply xl:(px-4);
+}
+</style>
